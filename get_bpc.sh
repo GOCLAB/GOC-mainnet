ac617d1b324a104cb1aff58c094270c8062380aa
GOCLAB <goclab2018@gmail.com>
2019-03-12 13:16:38 +0800

    add bp accounts and aotu generated script

---
+++ get_bpc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# get the BP candidate account name and the keys
+if [ -f ./bp_accounts.txt ]
+    then
+        rm ./bp_accounts.txt
+fi
+
+echo "# This txt list producer candidate accounts that will be created once GOC launch, please check" >> ./bp_accounts.txt
+echo "# If you want to change or add your key, just modify your bp.ini in producer_info file, this document will auto updated" >> ./bp_accounts.txt
+echo -e "# Format: producername-ownerkey-activekey(you can ingnore activekey)\n" >> ./bp_accounts.txt
+FILENAME="producer-info/"
+BPS="$(ls $FILENAME)"
+
+for f in $BPS
+do
+    name="$(cat $FILENAME$f | grep "producer-name" | awk '{print $3}')"
+    owner="$(cat $FILENAME$f | grep "Owner-key" | awk '{print $3}')"
+    active="$(cat $FILENAME$f | grep "Active-key" | awk '{print $3}')"
+    if [ -n "$(echo $active)" ]
+        then
+            echo $name-$owner-$active >> ./bp_accounts.txt
+        else
+            echo $name-$owner >> ./bp_accounts.txt
+    fi
+done
+
